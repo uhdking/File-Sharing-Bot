aa14278932a9264c1e1206ec60bccac4ad4b680e
uhdking <124318949+uhdking@users.noreply.github.com>
2023-03-24 09:55:49 +0530

    Update config.py

--- config.py
+++ config.py
@@ -22,7 +22,7 @@ OWNER_ID = int(os.environ.get("OWNER_ID", "1925422909"))
 PORT = os.environ.get("PORT", "8080")
 
 #Database 
-DB_URI = os.environ.get("DATABASE_URL", "mongodb+srv://uhdprime:uhdprime@cluster0.w3sxrux.mongodb.net/?retryWrites=true&w=majority")
+DB_URI = os.environ.get("DATABASE_URL", "mongodb+srv://uhdprime:uhdprime@imaxfilter.xsqdm6z.mongodb.net/?retryWrites=true&w=majority")
 DB_NAME = os.environ.get("DATABASE_NAME", "filesharexbot")
 
 #force sub channel id, if you want enable force sub
